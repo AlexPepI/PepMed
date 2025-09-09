e43808110a52b9037df0309b9d7a7ff73e288246
AlexPepl <alexandrospeponis1999@gmail.com>
2025-09-09 18:05:06 +0300

    Routes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,18 +5,23 @@ import {BrowserRouter as Router,Routes,Route} from "react-router";
 
 const Home = lazy(() => import("./pages/Home"));
 const NewVisitor = lazy(()=>import("./pages/NewVisitor"))
+const NewVisit = lazy(()=>import("./pages/NewVisit"))
+const VisitorDetails = lazy(()=> import("./pages/VisitorDetails"))
+const VisitDetails = lazy(()=> import("./pages/VisitDetails.tsx"))
+
 
 export function AppRoutes() {
     return(
         <Suspense fallback={<Loading />}>
             <Router>
                 <Routes>
-
                     <Route element={<Layout/>}>
                         <Route path="/" element={<Home />} />
                         <Route path="/new-visitor" element={<NewVisitor/>}/>
+                        <Route path="/new-visit" element={<NewVisit/>}/>            
+                        <Route path="/visitor-details/:id" element={<VisitorDetails/>}/>
+                        <Route path="/visit-details/:id" element={<VisitDetails/>}/>                        
                     </Route>
-                    
                     <Route path="*" element={<div className="p-6">Not found</div>} />
                 </Routes>
             </Router>
